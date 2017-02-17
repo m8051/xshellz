c9197f34ec210a0f7f328a8a74842595ff9b8ad4
Jordi Garcia <jgarciajimenez@groupon.com>
2017-02-17 19:59:31 +0000

    fix indent user.rb

--- user.rb
+++ user.rb
@@ -4,19 +4,19 @@ require 'json'
 class User
 
   attr_reader :message
-
+  
   def initialize(api, user, met="shellexpires")
     @api = api
     @user = user
     @met = met
-
     request
   end
-
+  
   def request
     response = HTTParty.get("#{@api}#{@user}/#{@met}")
     @message = response.parsed_response["msg"]
   end
+  
   def to_s
     "#{@message}"
   end
